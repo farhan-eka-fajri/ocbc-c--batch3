0e0fad598a501d8041d420dcabe10a739c6dddc9
Farhan Eka Fajri <farhanekaf@gmail.com>
2022-03-25 13:44:32 +0700

    add sesi9

---
+++ sesi9/Kantor_WebAPI/Kantor_WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Kantor_WebAPI.Models;
+
+namespace Kantor_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            this._context = context;
+        }
+
+        //GET: api/User
+
+        [HttpGet(Name = "Get Employee")]
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItems()
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+
+            return _context.GetAllEmployee();
+        }
+
+        // Get : api/user/{id}
+        [HttpGet("{id}", Name = "Get Employee Where")]
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItem(String id)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof (EmployeeContext)) as EmployeeContext;
+            return _context.GetEmployee(id);
+        }
+
+    }
+}
